3b298f672f9ee5c804cbc5a383b405e3803bbe18
Raziel Gartsman <raziel.gartsman@gmail.com>
2024-07-21 00:13:35 +0300

    milestone_communities: Embed MK name information

--- milestone_communities/main.py
+++ milestone_communities/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import networkx as nx
 import itertools
 
-from data.datasets import get_all_votes, VoteResultType
+from data.datasets import get_all_votes, VoteResultType, get_all_vip_ids
 from visualization import plot_graph_with_color
 
 
@@ -13,6 +13,8 @@ def pivot_results(vote_results: pd.DataFrame, allowed_vote_values: set, min_vote
     where vote_result is -1 if `kmmbr_id` did not participate in vote `vote_id`.
     Ignores rows with vote results not in `allowed_vote_values`, or rows describing vote_ids that appear less than
     `min_votes` times.
+    :return: A tuple of the pivot result as a numpy array, and a list translating this matrix index to the original
+    kmmbr_id
     """
     vote_results = vote_results[vote_results["vote_result"].isin(allowed_vote_values)]
 
@@ -23,10 +25,10 @@ def pivot_results(vote_results: pd.DataFrame, allowed_vote_values: set, min_vote
     pivot = vote_results.pivot_table(  # TODO assumes unique rows
         index="kmmbr_id", columns="vote_id", values="vote_result", aggfunc="sum", fill_value=-1)
 
-    return pivot.to_numpy()
+    return pivot.to_numpy(), list(pivot.index)
 
 
-def create_connection_graph(tristate_vote_matrix: np.array) -> np.array:
+def compute_connections(tristate_vote_matrix: np.array) -> np.array:
     """
     Creates a weighted graph representing the strength of connection between every pair of MKs, based on
     their given voting statistics.
@@ -53,13 +55,38 @@ def create_connection_graph(tristate_vote_matrix: np.array) -> np.array:
     return weighted_agreement_matrix
 
 
-def detect_communities(connections_matrix: np.array) -> np.array:
+def create_connection_graph(
+        connections_matrix: np.array, vip_ids: list[int], vip_ids_and_names_dataframe: pd.DataFrame) -> nx.Graph:
+    """
+    :param connections_matrix: Adjacency matrix, as returned by `create_connection_graph()`
+    :param vip_ids: Translation from matrix index to kmmbr_id.
+    :return: Input as networkx Graph()
+    """
     n, n_ = connections_matrix.shape
-    assert n == n_
+    assert n == n_ and n == len(vip_ids)
 
-    G: nx.Graph = nx.generators.complete_graph(n)
+    df = vip_ids_and_names_dataframe
+    df["full_name"] = df.apply(
+        lambda row: f"{row['mk_individual_first_name_eng']} {row['mk_individual_name_eng']}", axis=1)
+    filtered_df = df[df["vip_id"].isin(vip_ids)]
+    full_names = filtered_df.set_index("vip_id").loc[vip_ids]["full_name"].tolist()
+
+    nodes = [(vip_id, {"name": full_name}) for vip_id, full_name in zip(vip_ids, full_names)]
+
+    G: nx.Graph = nx.Graph()
+    G.add_nodes_from(nodes)
     G.add_weighted_edges_from(
-        ((i, j, connections_matrix[i, j]) for i, j in itertools.product(range(n), repeat=2) if i < j))
+        ((vip_ids[i], vip_ids[j], connections_matrix[i, j]) for i, j in itertools.product(
+            range(n), repeat=2) if i < j))
+
+    return G
+
+
+def detect_communities(G: nx.Graph) -> list[set[int]]:
+    """
+    :param G: Graph representation of MKs
+    :return: Louvain communities, see networkx's `community.louvain_communities()`
+    """
     return nx.community.louvain_communities(G)
 
 
@@ -73,7 +100,7 @@ def main():
     twenty_third_knesset_votes = all_votes[all_votes["vote_id"].isin(twenty_third_knesset_vote_ids)]
 
     # swap to MK x MK aggregation. `kmmbr_id`s are lost but can be easily reconstructed.
-    pivot: np.array = pivot_results(
+    pivot, idx_to_vip_id = pivot_results(
         twenty_third_knesset_votes,
         allowed_vote_values={VoteResultType.FOR.value, VoteResultType.AGAINST.value},
         min_votes=15,
@@ -82,10 +109,12 @@ def main():
     # +/-1 for support/oppose, 0 otherwise
     as_tristate: np.array = np.zeros_like(pivot, dtype=int) + (pivot == VoteResultType.FOR.value) - (
             pivot == VoteResultType.AGAINST.value)
-    connection_matrix = create_connection_graph(as_tristate)
+    connection_matrix = compute_connections(as_tristate)
 
     # list of index sets representing the communities
-    communities: list[set[int]] = detect_communities(connection_matrix)
+    vip_ids_and_names_dataframe = get_all_vip_ids()
+    G: nx.Graph = create_connection_graph(connection_matrix, idx_to_vip_id, vip_ids_and_names_dataframe)
+    communities: list[set[int]] = detect_communities(G)
 
     plot_graph_with_color(connection_matrix, communities)
 
