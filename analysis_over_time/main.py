64805a97d8e67dddc659276687eea781c21ccd65
Raziel Gartsman <raziel.gartsman@gmail.com>
2024-09-28 19:52:16 +0300

    analysis_over_time: More plot improvements

--- analysis_over_time/main.py
+++ analysis_over_time/main.py
@@ -55,7 +55,10 @@ def iter_by_timeframe(
         relevant_vote_results = vote_results[vote_results["vote_id"].isin(vote_ids)]
 
         knessets = relevant_vote_details["knesset_num"].unique()
-        if len(knessets) != 1:
+        if len(knessets) == 0:
+            start += interval
+            continue
+        elif len(knessets) != 1:
             raise ValueError(f"The timeframe {start}-{end} spans across multiple Knessets {knessets}")
 
         yield start, relevant_vote_details, relevant_vote_results, knessets[0]
@@ -65,10 +68,12 @@ def iter_by_timeframe(
 def plot_division(
         division_index_vals: list[tuple[int | str, float]],
         title: str, xlabel: str, ylabel: str = "Division Index",
-        xticks=None,
+        xticks: list[str] = None,
+        occasions: list[tuple[int, str]] = None,
     ):
     x, y = zip(*division_index_vals)
-    plt.plot(x, y, "ro")
+    plt.bar([_x + .5 for _x in x], y, width=1, color="tab:blue", edgecolor="black", linewidth=.5)
+    plt.ylim(.3, 1.)
     plt.title(title, size=20)
     plt.xlabel(xlabel, size=15)
     plt.ylabel(ylabel, size=15)
@@ -80,6 +85,23 @@ def plot_division(
         plt.xticks(size=15)
 
     plt.yticks(size=15)
+
+    if occasions:
+        ytext_off = [0, -.15, -.3]
+        y_off = [0, -.15, -.10]
+        for i, (occ_x, occ_label) in enumerate(occasions):
+            plt.axvline(x=occ_x, color="black", linestyle='--')
+            plt.annotate(
+                occ_label,
+                xy=(occ_x, .82 + y_off[i]),
+                xytext=(occ_x - 1.3, .9 + ytext_off[i]),
+                bbox=dict(facecolor="white", alpha=0.8, linewidth=.5),
+                arrowprops=dict(arrowstyle="->", color="black"),
+                fontsize=14,
+                color='black',
+            )
+
+    plt.rc("axes", axisbelow=True)
     plt.grid()
     plt.tight_layout()
     plt.savefig(f"division_{title.lower().replace(' ', '_')}.png")
@@ -90,7 +112,9 @@ def plot_around_occasion_monthly(
         start: datetime.datetime, end: datetime.datetime,
         vote_details: pd.DataFrame, vote_results: pd.DataFrame,
         coalition_opposition_by_knesset: dict[int, pd.DataFrame],
-        title: str, xlabel:str = "Month", ylabel: str = "Division Index"):
+        title: str, xlabel:str = "Month", ylabel: str = "Division Index",
+        occasions: list[tuple[float, str]] = None,
+):
     xticks = list()
     interval = datetime.timedelta(days=31)
     curr_tick = start
@@ -98,9 +122,11 @@ def plot_around_occasion_monthly(
         xticks.append(curr_tick.strftime("%m/%Y"))
         curr_tick += interval
 
-    index_vals = [(date.month, compute_division_index(res, coalition_opposition_by_knesset[knesset])) for (
-        date, _, res, knesset) in iter_by_timeframe(vote_details, vote_results, start, end, interval)]
-    plot_division(index_vals, title=title, xlabel=xlabel, ylabel=ylabel, xticks=xticks)
+    index_vals = [
+        (int((date - start).days / 30) + start.month, compute_division_index(res, coalition_opposition_by_knesset[knesset])) for (
+        date, _, res, knesset) in iter_by_timeframe(vote_details, vote_results, start, end, interval)
+    ]
+    plot_division(index_vals, title=title, xlabel=xlabel, ylabel=ylabel, xticks=xticks, occasions=occasions)
 
 
 def main():
@@ -123,14 +149,20 @@ def main():
         lebanon2_start, lebanon2_end, all_vote_details, all_vote_results,
         coalition_opposition_by_knesset,
         title="Second Lebanon War",
+        occasions=[(7 + 12 / 31, "War breaks out"), (8 + 14 / 30, "Ceasfire starts")]
     )
 
     protective_edge_start = datetime.datetime(2014, 5, 1)
-    protective_edge_end = datetime.datetime(2014, 12, 1)
+    protective_edge_end = datetime.datetime(2014, 11, 1)
     plot_around_occasion_monthly(
         protective_edge_start, protective_edge_end, all_vote_details, all_vote_results,
         coalition_opposition_by_knesset,
         title="Operation Protective Edge",
+        occasions=[
+            (6 + 12 / 30, 'Operation "Shuvu Ahim"'),
+            (7 + 17 / 31, "Ground Invasion Begins"),
+            (8 + 26 / 30, "Effective Ceasefire"),
+        ]
     )
 
     disengagement_start = datetime.datetime(2005, 1, 1)
@@ -139,6 +171,31 @@ def main():
         disengagement_start, disengagement_end, all_vote_details, all_vote_results,
         coalition_opposition_by_knesset,
         title="The Disengagement from Gaza Strip",
+    )
+
+    covid_start = datetime.datetime(2020, 2, 1)
+    covid_end = datetime.datetime(2020, 12, 1)
+    plot_around_occasion_monthly(
+        covid_start, covid_end, all_vote_details, all_vote_results,
+        coalition_opposition_by_knesset,
+        title="Covid Pandemic",
+        occasions=[
+            (3 + 20 / 31, "First Lockdown"),
+            (5 + 3 / 31, "Schools Reopen"),
+            (9 + 18 / 30, "Second Lockdown"),
+        ]
+    )
+
+    subprime_start = datetime.datetime(2008, 7, 1)
+    subprime_end = datetime.datetime(2009, 1, 1)
+    plot_around_occasion_monthly(
+        subprime_start, subprime_end, all_vote_details, all_vote_results,
+        coalition_opposition_by_knesset,
+        title="Subprime Crisis",
+        occasions=[
+            (9 + 15 / 30, "Lehman Brothers defunct"),
+            (12 + 25 / 31, "Tel Aviv 100 lowest point"),
+        ]
     )
 
 
